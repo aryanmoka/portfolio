ef9436885f07c00e9b7bde59aa247fc267e529ff
aryanmoka <aryanmokashi28@gmail.com>
2026-08-30 19:59:08 +0530

    Replace portfolio with updated version

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -74,7 +74,7 @@ const Certificates: React.FC = () => {
       date: '2026'
     },
     {
-      id: 'CERT-003',
+      id: 'CERT-007',
       title: 'Basic to Advance Tableau',
       issuer: 'Skill Nation',
       link: 'https://excel.jatanshah.in/your-certificate/2D1691609115-2D16916091DC-2D089EE0DD63/',
